e85c34247503cf0fecca68412ab47e113caf99aa
parampreet.singh <parampreet.singh@tothenew.com>
2015-11-26 18:40:25 +0530

    barrier-print

--- src/Barrier.java
+++ src/Barrier.java
@@ -14,11 +14,7 @@ public class Barrier {
                 while (!done) {
                     i++;
                     int j = 0;
-                    try {
-                        Thread.sleep(500);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+                    System.out.print("blah!!");
                 }
                 System.out.println("Done !!");
             }
