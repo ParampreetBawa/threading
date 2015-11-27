482ebdfbb15c02d0254a20907e769f0b62bc503c
parampreet.singh <parampreet.singh@tothenew.com>
2015-11-27 21:27:16 +0530

    commit

--- src/Example.groovy
+++ src/Example.groovy
@@ -5,15 +5,24 @@ class Example {
 
     final Object lockA = new Object();
     final Object lockB = new Object();
+
+
+    public static void main(String[] args) {
+        final Example example = new Example()
+        Thread.start("Thread A") { example.methodA() }
+        Thread.start("Thread B") { example.methodB() }
+    }
     void methodA() {
         synchronized (lockA) {
-
+            println("Method A -  " + Thread.currentThread().name)
+            Thread.sleep(5000)
         }
     }
 
     void methodB() {
         synchronized (lockB) {
-
+            println("Method B -  " + Thread.currentThread().name)
+            Thread.sleep(5000)
         }
 
     }
