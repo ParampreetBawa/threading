c1006a041c1689cd6c234ea974eefe537313ee78
parampreet.singh <parampreet.singh@tothenew.com>
2016-02-26 19:21:58 +0530

    commint some implementation

--- src/Example.groovy
+++ src/Example.groovy
@@ -3,11 +3,29 @@
  */
 class Example {
     public static void main(String[] args) {
+        final Example example = new Example()
+        new Thread(new Runnable() {
+            @Override
+            void run() {
+                while(true)
+                    example.method()
+            }
+        },"A").start();
+
+        new Thread(new Runnable() {
+            @Override
+            void run() {
+                while(true) {
+                    example.method();
+                }
+            }
+        },"B").start()
 
     }
 
     synchronized void method() {
-
+        println(Thread.currentThread().name)
+        Thread.sleep(5000)
     }
 
 }
