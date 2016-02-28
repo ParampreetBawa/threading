91fad9b8c601f3ec2b1059472950a5d818d4edbb
parampreet.singh <parampreet.singh@tothenew.com>
2016-02-28 11:10:59 +0530

    fork example

--- src/Example.groovy
+++ src/Example.groovy
@@ -1,30 +1,68 @@
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
+
+import java.util.concurrent.ForkJoinPool
+import java.util.concurrent.ForkJoinTask
+import java.util.concurrent.RecursiveTask
+
 /**
  * Created by parampreet on 11/24/15.
  */
-class Example {
+@Grab('org.jsoup:jsoup:1.7.1')
+ForkJoinPool forkJoinPool = new ForkJoinPool(Runtime.getRuntime().availableProcessors());
+SiteMapTask siteMapTask = new SiteMapTask("http://www.tothenew.com");
 
-    final Object lockA = new Object();
-    final Object lockB = new Object();
+forkJoinPool.invoke(siteMapTask);
+class SiteMapTask extends RecursiveTask<List<String>> {
 
+    String url
 
-    public static void main(String[] args) {
-        final Example example = new Example()
-        Thread.start("Thread A") { example.methodA() }
-        Thread.start("Thread B") { example.methodB() }
+    public SiteMapTask(String url) {
+        this.url = url
     }
-    void methodA() {
-        synchronized (lockA) {
-            println("Method A -  " + Thread.currentThread().name)
-            Thread.sleep(5000)
+
+    @Override
+    protected List<String> compute() {
+        List<String> linksOnPage = collectLinks()
+
+        if (linksOnPage.size() == 0) {
+            return linksOnPage
+        } else {
+            List<SiteMapTask> subTasks = divide(linksOnPage)
+            List<ForkJoinTask> forkJoinTaskList = new ArrayList<>();
+            for(SiteMapTask subTask : subTasks){
+                forkJoinTaskList.add(subTask.fork());
+            }
+
+            List<String> links = new ArrayList<>();
+            for(ForkJoinTask subTask : forkJoinTaskList) {
+                links.addAll(subTask.join());
+            }
         }
     }
 
-    void methodB() {
-        synchronized (lockB) {
-            println("Method B -  " + Thread.currentThread().name)
-            Thread.sleep(5000)
-        }
+    protected List<SiteMapTask> divide(List<String> links) {
+        List<SiteMapTask> subTasks = new ArrayList<>();
+        for(String link: links)
+            subTasks.add(new SiteMapTask<>(link))
 
+        subTasks
     }
 
+    protected List<String> collectLinks() {
+        List<String> pageLinks = new ArrayList<>();
+        try {
+            Document doc = Jsoup.connect(url).get();
+            Elements links = doc.select("a[href]");
+            for (Element link : links) {
+                println(link)
+                pageLinks.add(link.attr("href"))
+            }
+        }catch (Exception e) {
+            //ok for now
+        }
+        return pageLinks
+    }
 }
