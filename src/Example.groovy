915ccf40b6c6e5bc9634715b779dfb909fd44d42
parampreet.singh <parampreet.singh@tothenew.com>
2017-04-11 14:38:49 +0530

    example complete

--- src/Example.groovy
+++ src/Example.groovy
@@ -1,3 +1,4 @@
+import java.util.concurrent.Callable
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import java.util.concurrent.Future
@@ -13,21 +14,28 @@ class Example {
 
         ExecutorService service = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors())
 
-        Runnable r1 = new Runnable() {
-            public void run() {
-                sleep(100);
+        Callable r1 = new Callable() {
+            public Object call() {
+                new URL("https://facebook.com").text
             }
         }
 
         long t1 = System.currentTimeMillis();
         Future future = service.submit(r1);
         try {
-            future.get(10, TimeUnit.MILLISECONDS)
+            def text = future.get(1, TimeUnit.MINUTES)
+            saveToFile(text)
         }catch (TimeoutException e) {
             System.out.println("Exception")
         }
         System.out.println(System.currentTimeMillis() - t1)
 
         service.shutdown()
+    }
+
+    static void saveToFile(text) {
+        def file = File.createTempFile("fab", ".html")
+        file.text = text
+        System.out.println(file.getAbsolutePath())
     }
 }
